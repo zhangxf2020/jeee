eb22bd4afa60d123061ca6f7258bb002c48481f3
zhangxf2020 <fan102684@163.com>
2020-03-10 18:02:09 +0800

    默认更改列表

---
+++ pyQT/综合案例/Register_Pane.py
@@ -0,0 +1,62 @@
+from PyQt5.Qt import *
+import sys
+from Resources.register import Ui_Form
+
+class RegisterPane(QWidget,Ui_Form):
+    exit_signal = pyqtSignal()
+    register_account_pwd_signal = pyqtSignal(str,str)
+    def __init__(self):
+        super(RegisterPane, self).__init__()
+        self.setAttribute(Qt.WA_StyledBackground,True)
+        self.setupUi(self)
+        self.animation_targets =[self.about_menue_btn,self.reset_menue_btn,self.exit_menue_btn]
+        self.animation_targets_pos = [target.pos() for target in self.animation_targets]
+
+    def show_hide_menue(self,checked):
+        animation_group = QSequentialAnimationGroup(self)#串行组动画,一个播放完之后再播放另一个
+        for idx,target in enumerate(self.animation_targets):#enumerate返回一个序列的下标和对应的元素
+            animation = QPropertyAnimation()#使用属性动画,这里更改坐标
+            animation.setTargetObject(target)
+            animation.setPropertyName(b'pos')
+
+            animation.setStartValue(self.animation_targets_pos[idx])#因为一开始是展开的,所以一开始的坐标为按钮的坐标
+            animation.setEndValue(self.main_menue_btn.pos())#回收到菜单按钮的坐标
+
+            animation.setDuration(200)#设置动画时长
+            animation.setEasingCurve(QEasingCurve.OutBounce)#是指动画弹簧效果
+            animation_group.addAnimation(animation)#往动画组内添加动画
+        if checked:
+            animation_group.setDirection(QAbstractAnimation.Backward)#如果是按下状态,则需要收回所以是反着执行动画
+        else:
+            animation_group.setDirection(QAbstractAnimation.Forward)
+        animation_group.start(QAbstractAnimation.DeleteWhenStopped)
+
+
+    def about_lk(self):
+        QMessageBox.about(self,'综合案例','www.baiducom')
+    def reset(self):
+        self.account_le.clear()
+        self.password_le.clear()
+        self.confirm_pwd_le.clear()
+    def exit_pane(self):
+        self.exit_signal.emit()
+    def check_register(self):
+        account_txt = self.account_le.text()
+        password_txt = self.password_le.text()
+        self.register_account_pwd_signal.emit(account_txt,password_txt)
+    def enable_register_btn(self):
+        account_txt = self.account_le.text()
+        password_txt = self.password_le.text()
+        cp_txt = self.confirm_pwd_le.text()
+        if len(account_txt)>0 and len(password_txt)>0 and len(cp_txt)>0 and password_txt ==\
+                cp_txt:
+            return True
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = RegisterPane()
+    win.show()
+    sys.exit(app.exec_())
---
+++ pyQT/综合案例/Resources/register.py
@@ -0,0 +1,219 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'register.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(500, 450)
+        Form.setMinimumSize(QtCore.QSize(500, 450))
+        Form.setMaximumSize(QtCore.QSize(500, 450))
+        Form.setStyleSheet("QWidget#Form{\n"
+"border-image: url(:/register/images/register_background.jpg);}")
+        self.main_menue_btn = QtWidgets.QPushButton(Form)
+        self.main_menue_btn.setGeometry(QtCore.QRect(10, 20, 40, 40))
+        self.main_menue_btn.setStyleSheet("QPushButton {\n"
+"    border-radius:20px;\n"
+"    color:white;\n"
+"    background-color:rgb(103, 167, 193);\n"
+"    border:2px solid rgb(186, 229, 240)\n"
+"        \n"
+"}\n"
+"QPushButton:hover {\n"
+"    \n"
+"    border:3px double rgb(2, 255, 120)\n"
+"        \n"
+"}\n"
+"QPushButton:checked {\n"
+"    \n"
+"    background-color:rgb(20, 212, 255);\n"
+"        \n"
+"}")
+        self.main_menue_btn.setCheckable(True)
+        self.main_menue_btn.setChecked(True)
+        self.main_menue_btn.setObjectName("main_menue_btn")
+        self.about_menue_btn = QtWidgets.QPushButton(Form)
+        self.about_menue_btn.setGeometry(QtCore.QRect(80, 10, 40, 40))
+        self.about_menue_btn.setStyleSheet("QPushButton {\n"
+"    border-radius:20px;\n"
+"    color:white;\n"
+"    background-color:rgb(103, 167, 193);\n"
+"    border:2px solid rgb(186, 229, 240)\n"
+"        \n"
+"}\n"
+"QPushButton:hover {\n"
+"    \n"
+"    border:3px double rgb(2, 255, 120)\n"
+"        \n"
+"}\n"
+"QPushButton:checked {\n"
+"    \n"
+"    background-color:rgb(20, 212, 255);\n"
+"        \n"
+"}")
+        self.about_menue_btn.setCheckable(False)
+        self.about_menue_btn.setObjectName("about_menue_btn")
+        self.reset_menue_btn = QtWidgets.QPushButton(Form)
+        self.reset_menue_btn.setGeometry(QtCore.QRect(70, 60, 40, 40))
+        self.reset_menue_btn.setStyleSheet("QPushButton {\n"
+"    border-radius:20px;\n"
+"    color:white;\n"
+"    background-color:rgb(103, 167, 193);\n"
+"    border:2px solid rgb(186, 229, 240)\n"
+"        \n"
+"}\n"
+"QPushButton:hover {\n"
+"    \n"
+"    border:3px double rgb(2, 255, 120)\n"
+"        \n"
+"}\n"
+"QPushButton:checked {\n"
+"    \n"
+"    background-color:rgb(20, 212, 255);\n"
+"        \n"
+"}")
+        self.reset_menue_btn.setCheckable(False)
+        self.reset_menue_btn.setObjectName("reset_menue_btn")
+        self.exit_menue_btn = QtWidgets.QPushButton(Form)
+        self.exit_menue_btn.setGeometry(QtCore.QRect(20, 80, 40, 40))
+        self.exit_menue_btn.setStyleSheet("QPushButton {\n"
+"    border-radius:20px;\n"
+"    color:white;\n"
+"    background-color:rgb(103, 167, 193);\n"
+"    border:2px solid rgb(186, 229, 240)\n"
+"        \n"
+"}\n"
+"QPushButton:hover {\n"
+"    \n"
+"    border:3px double rgb(2, 255, 120)\n"
+"        \n"
+"}\n"
+"QPushButton:checked {\n"
+"    \n"
+"    background-color:rgb(20, 212, 255);\n"
+"        \n"
+"}")
+        self.exit_menue_btn.setCheckable(False)
+        self.exit_menue_btn.setObjectName("exit_menue_btn")
+        self.widget = QtWidgets.QWidget(Form)
+        self.widget.setGeometry(QtCore.QRect(200, 230, 231, 207))
+        self.widget.setObjectName("widget")
+        self.formLayout = QtWidgets.QFormLayout(self.widget)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.formLayout.setVerticalSpacing(15)
+        self.formLayout.setObjectName("formLayout")
+        self.label = QtWidgets.QLabel(self.widget)
+        self.label.setStyleSheet("color:rgb(255, 85, 0);\n"
+"\n"
+"font: 12pt \"楷体\";")
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label)
+        self.account_le = QtWidgets.QLineEdit(self.widget)
+        self.account_le.setMinimumSize(QtCore.QSize(0, 30))
+        self.account_le.setStyleSheet("background-color:transparent;\n"
+"color:rgb(243, 243, 243);\n"
+"border:None;\n"
+"border-bottom:1px solid lightgray;")
+        self.account_le.setText("")
+        self.account_le.setClearButtonEnabled(True)
+        self.account_le.setObjectName("account_le")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.account_le)
+        self.label_2 = QtWidgets.QLabel(self.widget)
+        self.label_2.setStyleSheet("color:rgb(255, 85, 0);\n"
+"\n"
+"font: 12pt \"楷体\";")
+        self.label_2.setObjectName("label_2")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_2)
+        self.password_le = QtWidgets.QLineEdit(self.widget)
+        self.password_le.setMinimumSize(QtCore.QSize(0, 30))
+        self.password_le.setStyleSheet("background-color:transparent;\n"
+"color:rgb(243, 243, 243);\n"
+"border:None;\n"
+"border-bottom:1px solid lightgray;")
+        self.password_le.setClearButtonEnabled(True)
+        self.password_le.setObjectName("password_le")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.password_le)
+        self.label_3 = QtWidgets.QLabel(self.widget)
+        self.label_3.setStyleSheet("color:rgb(255, 85, 0);\n"
+"\n"
+"font: 12pt \"楷体\";")
+        self.label_3.setObjectName("label_3")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.label_3)
+        self.confirm_pwd_le = QtWidgets.QLineEdit(self.widget)
+        self.confirm_pwd_le.setMinimumSize(QtCore.QSize(0, 30))
+        self.confirm_pwd_le.setStyleSheet("background-color:transparent;\n"
+"color:rgb(243, 243, 243);\n"
+"border:None;\n"
+"border-bottom:1px solid lightgray;")
+        self.confirm_pwd_le.setClearButtonEnabled(True)
+        self.confirm_pwd_le.setObjectName("confirm_pwd_le")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.confirm_pwd_le)
+        self.pushButton = QtWidgets.QPushButton(self.widget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setMinimumSize(QtCore.QSize(0, 40))
+        self.pushButton.setStyleSheet("QPushButton {\n"
+"background-color: rgb(255, 170, 127);\n"
+"    color:rgb(14, 131, 174);\n"
+"    border-radius:10px\n"
+"}\n"
+"QPushButton:hover {\n"
+"background-color: rgb(255, 192, 167);\n"
+"}\n"
+"QPushButton:pressed {\n"
+"background-color: rgb(195, 130, 97);\n"
+"}")
+        self.pushButton.setObjectName("pushButton")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.SpanningRole, self.pushButton)
+        self.label.raise_()
+        self.label_2.raise_()
+        self.label_3.raise_()
+        self.account_le.raise_()
+        self.confirm_pwd_le.raise_()
+        self.password_le.raise_()
+        self.pushButton.raise_()
+        self.about_menue_btn.raise_()
+        self.reset_menue_btn.raise_()
+        self.exit_menue_btn.raise_()
+        self.main_menue_btn.raise_()
+
+        self.retranslateUi(Form)
+        self.main_menue_btn.clicked['bool'].connect(Form.show_hide_menue)
+        self.about_menue_btn.clicked.connect(Form.about_lk)
+        self.reset_menue_btn.clicked.connect(Form.reset)
+        self.exit_menue_btn.clicked.connect(Form.exit_pane)
+        self.pushButton.clicked.connect(Form.check_register)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.main_menue_btn.setText(_translate("Form", "菜单"))
+        self.about_menue_btn.setText(_translate("Form", "关于"))
+        self.reset_menue_btn.setText(_translate("Form", "重置"))
+        self.exit_menue_btn.setText(_translate("Form", "退出"))
+        self.label.setText(_translate("Form", "账    号:"))
+        self.label_2.setText(_translate("Form", "密    码:"))
+        self.label_3.setText(_translate("Form", "确认密码:"))
+        self.pushButton.setText(_translate("Form", "注册"))
+
+import images_rc
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
